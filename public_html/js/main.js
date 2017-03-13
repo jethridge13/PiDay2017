59e0100eabb06ca1532db55bc2cb72882e621a90
Joshua Ethridge <joshua.ethridge13@gmail.com>
2017-03-13 15:53:24 -0400

    Initial Commit

---
+++ public_html/js/main.js
@@ -0,0 +1,9 @@
+// Open the side help bar
+function openNav() {
+    document.getElementById("sidenav").style.width = "250px";
+}
+
+// Close the side help bar
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+}
